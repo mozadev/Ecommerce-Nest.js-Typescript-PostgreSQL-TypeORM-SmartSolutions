3076a7aee9f481667e9323ba793dc6546a38173c
ceosmore <ceosmore@gmail.com>
2024-06-06 14:11:18 -0500

    chore: Update package.json and app.module.ts with TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+    }),
+  ],
 })
 export class AppModule {}
