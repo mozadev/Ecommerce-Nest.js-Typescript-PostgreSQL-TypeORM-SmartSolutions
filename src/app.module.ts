4404fde65e11caba2dc29a3a0ca0403bd01a8621
ceosmore <ceosmore@gmail.com>
2024-06-06 17:38:47 -0500

    chore: Update app.module.ts with TypeORM configuration and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
   ],
 })
