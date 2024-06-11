48512e180b3b6ee96a172c37fd3ff700e927d4e1
ceosmore <ceosmore@gmail.com>
2024-06-11 13:05:47 -0500

    feat: Add validation rules to CreateProductDto for title, price, description, slug, stock, sizes, and gender

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,9 +1,37 @@
+import {
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
   title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   price?: number;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsString()
+  @IsOptional()
   slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
   stock?: number;
+
+  @IsString({ each: true })
   sizes: string[];
   gender: string;
 }
