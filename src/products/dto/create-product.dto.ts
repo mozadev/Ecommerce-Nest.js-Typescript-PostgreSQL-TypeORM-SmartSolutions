99ecefcbf17d417e049ba6075041a89f17e64f4b
ceosmore <ceosmore@gmail.com>
2024-06-11 12:56:32 -0500

    feat: Add properties to CreateProductDto for title, price, description, slug, stock, sizes, and gender

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,9 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+  title: string;
+  price?: number;
+  description?: string;
+  slug?: string;
+  stock?: number;
+  sizes: string[];
+  gender: string;
+}
