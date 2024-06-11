4a857742958bd77d6076d88292efc5246c1a51ed
ceosmore <ceosmore@gmail.com>
2024-06-11 12:06:04 -0500

    chore: Update product entity with default price value

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -10,8 +10,16 @@ export class Product {
   })
   title: string;
 
+  // if this were a interface, we couldn't create decorators.
   @Column('numeric', {
     default: 0,
   })
   price: number;
+
+  //other form to define a column
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
 }
