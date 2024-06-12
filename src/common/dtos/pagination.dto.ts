c5ed482857661568f10d574b722fa4914d6a8741
ceosmore <ceosmore@gmail.com>
2024-06-11 22:11:46 -0500

    feat: Add class-transformer to PaginationDto for implicit conversion

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,12 +1,15 @@
+import { Type } from 'class-transformer';
 import { IsOptional, IsPositive } from 'class-validator';
 
 export class PaginationDto {
   @IsOptional()
   @IsPositive()
   //Transformar
+  @Type(() => Number) // enableImplicitConversion: true
   limit?: number;
 
   @IsOptional()
   @IsPositive()
+  @Type(() => Number) // enableImplicitConversion: true
   offset?: number;
 }
