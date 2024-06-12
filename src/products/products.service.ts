8f9cdcdcc03a19dafb06be41883aa77a2141a12f
ceosmore <ceosmore@gmail.com>
2024-06-11 22:21:10 -0500

    feat: Add pagination functionality to findAll method in ProductsService

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,11 +5,14 @@ import {
   Logger,
   NotFoundException,
 } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
+
+import { UpdateProductDto } from './dto/update-product.dto';
+import { CreateProductDto } from './dto/create-product.dto';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
@@ -32,8 +35,14 @@ export class ProductsService {
     }
   }
 
-  async findAll() {
-    return await this.productRepository.find({});
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    return await this.productRepository.find({
+      take: limit,
+      skip: offset,
+
+      // TODO: relations
+    });
   }
 
   async findOne(id: string) {
