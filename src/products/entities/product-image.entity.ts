610ac95a52f507aa3253f33953213f4b22b372af
ceosmore <ceosmore@gmail.com>
2024-06-13 13:34:27 -0500

    feat: Add ManyToOne relationship between ProductImage and Product entities

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Product } from './product.entity';
 
 @Entity()
@@ -9,5 +9,6 @@ export class ProductImage {
   @Column('text')
   url: string;
 
+  @ManyToOne(() => Product, (product) => product.images)
   product: Product;
 }
