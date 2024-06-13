48570eb27fa4a1a0223063b9ef23b79ee8873816
ceosmore <ceosmore@gmail.com>
2024-06-13 13:11:39 -0500

    feat: Refactor ProductsModule imports to use Product and ProductImage from entities. Create file of barrel

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,8 +3,7 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { Product } from './entities/product.entity';
-import { ProductImage } from './entities/product-image.entity';
+import { Product, ProductImage } from './entities';
 
 @Module({
   controllers: [ProductsController],
