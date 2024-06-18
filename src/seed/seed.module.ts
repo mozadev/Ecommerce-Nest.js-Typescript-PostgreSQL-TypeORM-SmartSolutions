9c2cf0b4815f22fd1be4fb085f70848118dc14c4
ceosmore <ceosmore@gmail.com>
2024-06-17 22:52:10 -0500

    chore: Add exports to ProductsModule

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { ProductsService } from 'src/products/products.service';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [ProductsService],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class SeedService {
   async runSeed() {
+    await this.insertNewProducts();
     return 'SEED EXECUTED';
+  }
+
+  private async insertNewProducts() {
+    //this.productService.deleteAllProducts();
+    return true;
   }
 }
