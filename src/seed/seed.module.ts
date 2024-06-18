6510af2f790d57cb50d1926eabf3e68e0957ba04
ceosmore <ceosmore@gmail.com>
2024-06-18 12:54:18 -0500

    chore: Update imports in seed module to use ProductsModule

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { ProductsService } from 'src/products/products.service';
+import { ProductsModule } from './../products/products.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [ProductsService],
+  imports: [ProductsModule],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ProductsService } from 'src/products/products.service';
 
 @Injectable()
 export class SeedService {
+  constructor(private readonly productsServices: ProductsService) {}
+
   async runSeed() {
     await this.insertNewProducts();
     return 'SEED EXECUTED';
   }
 
   private async insertNewProducts() {
-    //this.productService.deleteAllProducts();
+    await this.productsServices.deleteAllProducts();
     return true;
   }
 }
