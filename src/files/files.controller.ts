0be522798ff065a13f6bd55884ff17bb2e01ad49
ceosmore <ceosmore@gmail.com>
2024-06-19 14:09:30 -0500

    feat: Add @types/multer dependency

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -12,4 +12,10 @@ import { FilesService } from './files.service';
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  // express is difined globally in nestjs
+  uploadProductImage(file: Express.Multer.File) {
+    return 'hola mundo';
+  }
 }
