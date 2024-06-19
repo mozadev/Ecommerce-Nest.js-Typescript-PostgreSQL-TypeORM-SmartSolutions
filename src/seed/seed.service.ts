6a8b759e2a3dcd20888fa59f5c2e6859ec12696b
ceosmore <ceosmore@gmail.com>
2024-06-18 19:59:27 -0500

    feat: Insert new products during data seeding

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -14,6 +14,15 @@ export class SeedService {
   private async insertNewProducts() {
     await this.productsServices.deleteAllProducts();
     const products = initialData.products;
+    const insertPromises = [];
+
+    products.forEach((product) => {
+      // create resolv a promise with the product created
+      insertPromises.push(this.productsServices.create(product));
+    });
+
+    // wait for all the promises to be resolved
+    await Promise.all(insertPromises);
     return true;
   }
 }
