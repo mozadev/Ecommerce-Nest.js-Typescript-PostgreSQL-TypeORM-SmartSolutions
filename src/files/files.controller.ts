c3ba9b784bf96349d300ae47bee17b2f4b3ce93d
ceosmore <ceosmore@gmail.com>
2024-06-19 16:39:33 -0500

    feat: Update file upload functionality in FilesController

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -6,16 +6,20 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
-  @Post('product')
   // express is difined globally in nestjs
-  uploadProductImage(file: Express.Multer.File) {
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     return file;
   }
 }
