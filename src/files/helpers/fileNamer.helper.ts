3a0423ca0124f86d33f54e1f0dee5999b857c56d
ceosmore <ceosmore@gmail.com>
2024-06-20 13:40:29 -0500

    Add fileNamer.helper.ts to rename file

---
+++ src/files/helpers/fileNamer.helper.ts
@@ -0,0 +1,11 @@
+export const fileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  callback: Function,
+) => {
+  //   console.log({ file });
+  // this validation is innecesary, because the file is required. Let's leave it just in case
+  if (!file) return callback(new Error('No file'), false);
+
+  callback(null, false);
+};
