2b16b27b729b9e313c41481ba0421243bc443e92
ceosmore <ceosmore@gmail.com>
2024-06-20 13:36:08 -0500

    feat: Update file upload destination in FilesController

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -30,7 +30,7 @@ export class FilesController {
       fileFilter: fileFilter,
       // limits: { fileSize: 1000 },
       storage: diskStorage({
-        destination: './static/uploads',
+        destination: './static/products',
       }),
     }),
   )
