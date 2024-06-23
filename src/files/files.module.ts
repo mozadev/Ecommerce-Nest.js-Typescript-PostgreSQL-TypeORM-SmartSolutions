c56b5c402462402d87ab522d2a0e1054140cba76
ceosmore <ceosmore@gmail.com>
2024-06-23 00:58:30 -0500

    feat: Update file upload destination and file naming in FilesController

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [FilesController],
