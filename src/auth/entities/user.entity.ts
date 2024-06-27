7682596654225c4bb00f78c26190bfe59a0c91e5
mozadev <ceosmore@gmail.com>
2024-06-27 18:59:42 -0500

    refactor: Update User entity with additional fields and primary key

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,8 +1,20 @@
-import { Entity } from "typeorm";
+import { Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 import { initialData } from '../../seed/data/seed-data';
 
 
 @Entity('users')
 export class User {
+
+    @PrimaryGeneratedColumn('uuid')
     id: string;
+
+    email: string;
+
+    password: string;
+
+    fullname: string;
+
+    isActive: boolean;
+
+    roles: String[];
 }
