568eda555cd3575fed6460b3823f6b67a85dc89c
mozadev <ceosmore@gmail.com>
2024-06-28 14:13:27 -0500

    refactor: Update AuthService to use CreateUserDto in create method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 }
