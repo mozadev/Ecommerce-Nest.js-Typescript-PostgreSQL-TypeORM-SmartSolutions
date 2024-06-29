9c7427324761ac740e168d6e15d9f0b4baed391c
mozadev <ceosmore@gmail.com>
2024-06-29 10:19:06 -0500

    inject service ConfigService from ConfigModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,13 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User } from './entities/user.entity';
+
 
 @Module({
   controllers: [AuthController],
@@ -12,9 +15,10 @@ import { JwtModule } from '@nestjs/jwt';
   imports: [TypeOrmModule.forFeature([User]),
   PassportModule.register({defaultStrategy: 'jwt'}),
 
+  // inject service ConfigService from ConfigModule
   JwtModule.registerAsync({
-    imports: [],
-    inject: [],
+    imports: [ConfigModule],
+    inject: [ConfigService],
     useFactory:() => {
       console.log('process.env.JWT_SECRET', process.env.JWT_SECRET)
       return {
