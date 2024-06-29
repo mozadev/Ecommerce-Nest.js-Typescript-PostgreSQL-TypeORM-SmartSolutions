ec81a7eb8578f1224e65bf68323bdc3ea19d6ae7
mozadev <ceosmore@gmail.com>
2024-06-28 19:09:27 -0500

    chore: Add bcrypt dependency and update package.json

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { BadGatewayException, BadRequestException, Injectable, InternalServerErr
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
+import * as bcrypt from 'bcrypt'
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
@@ -17,7 +19,16 @@ export class AuthService {
   async create(createUserDto: CreateUserDto) {
 
     try {
-      const user = this.userRepository.create(createUserDto);
+
+      const {password, ...userData} = createUserDto;
+
+      const user = this.userRepository.create(
+
+        {
+          ...userData,
+          password: bcrypt.hashSync(password, 10)
+        }
+      );
 
       await this.userRepository.save(user);
 
