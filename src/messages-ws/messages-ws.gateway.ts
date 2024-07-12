ee33a5a9df604b7e7d0646b0f5952906f96a0b27
mozadev <ceosmore@gmail.com>
2024-07-11 22:25:12 -0500

    chore: Update WebSocketGateway options to enable CORS and add necessary dependencies

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,16 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway({cors: true})
-export class MessagesWsGateway {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+@WebSocketGateway({cors: true, namespace: 'messages' })
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(private readonly messagesWsService: MessagesWsService) { }
+  
+  handleDisconnect(client: Socket) {
+console.log('client disconnected', client.id);
+  }
+
+  handleConnection(client: Socket) {
+console.log('client connected', client.id );
+  }
 }
