c3932d84b24e75df40b21e1d97ec0cb10cde4edb
mozadev <ceosmore@gmail.com>
2024-07-11 19:42:24 -0500

    Change npm instead yarn. Documentation of Dtos, and Dto UpdateProductoDto that extends with PartialType of other Dto CreateProductDto

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,13 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'How many rows do you need',
+  })
   @IsOptional()
   @IsPositive()
   //Transformar
   @Type(() => Number) // enableImplicitConversion: true
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description: 'How many rows do you skip',
+  })
   @IsOptional()
   @Type(() => Number) // enableImplicitConversion: true
   offset?: number;
