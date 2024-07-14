e0d06a8f872adc2f76d51952f055972cae10a69c
mozadev <ceosmore@gmail.com>
2024-07-14 10:04:13 -0500

    feat: Register and track connected clients in Messages WebSocket service

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,15 +2,29 @@ import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nes
 import { MessagesWsService } from './messages-ws.service';
 import { Socket } from 'socket.io';
 
-@WebSocketGateway({cors: true, namespace: 'messages' })
+@WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(private readonly messagesWsService: MessagesWsService) { }
-  
-  handleDisconnect(client: Socket) {
-console.log('client disconnected', client.id);
-  }
+
+  constructor(
+    private readonly messagesWsService: MessagesWsService
+  ) { }
 
   handleConnection(client: Socket) {
-console.log('client connected', client.id );
+    // console.log('client connected', client.id);
+    this.messagesWsService.registerClient(client);
+
+    console.log({ conectados: this.messagesWsService.getConnectedClients() });
   }
+  
+  
+  handleDisconnect(client: Socket) {
+    // console.log('client disconnected', client.id);
+    this.messagesWsService.removedClient(client.id);
+    console.log({ conectados: this.messagesWsService.getConnectedClients() });
+  }
+
+
 }
+
+
+
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,36 @@
+
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+interface ConnectClients {
+    // socket id point to the socket instance
+    [id: string]: Socket;
+
+    // I haven't got limit of clients connected, also I can be save on database but it's volatile when reload the browser
+    // example id of client and socket instance
+    // 'sfsfdsusuario1': socket
+    // 'sfsfdsusuario2': socket
+    // 'sfsfdsa.....3': socket
+    // 'sfsfdsa.....4': socket
+
+}
 
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+
+    private connectedClients: ConnectClients = {};
+
+    registerClient(client: Socket) {
+        //clie nt id points to the socket instance that come from parameter
+        this.connectedClients[client.id] = client;  
+    }
+
+    removedClient(clientId: string) {
+        delete this.connectedClients[clientId];
+    }
+
+    getConnectedClients(): number {
+        return Object.keys(this.connectedClients).length;
+    }
+
+}
